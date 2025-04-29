e7b2352d5a44e79b5e281f9f2f51a02959e61486
SergioCaMi <sergiocami84@gmail.com>
2025-04-29 21:47:36 +0200

    done

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ const directory = path.join(__dirname, "subestaciones");
 
 
 
-fs.readdir(directory, (err, files) => {
+fs.readdir("subestaciones", (err, files) => {
   if (err) {
     console.log("Error al leer el directorio", err);
   } else {
     files.forEach((file) => {
-      const filePath = path.join(directory, file);
-      const stationInfo = fs.readFileSync(filePath, "utf-8");
+      const filePath =  path.join(__dirname, "subestaciones", file);
+      const stationInfo = fs.readFileSync(filePath, "utf-8").trim();
       const data = stationInfo.split(",");
-      const value1 = data[0].trim();
-      const value2 = data[1].trim();
-      const result = +value1 - (+value2);
+      const result = +data[0] - (+data[1]);
       if (result>10000){
-        console.log(`La estación que ha fallado es la ${file} con una pérdida de ${result} unidades`);
+        console.log(`⚡ Subestación con fallo detectada: ${file}`);
+        console.log(`Lecturas: ${data}`);
+        console.log(`Diferencia: ${data[0] - data[1]}`); 
     }
     });
   }
